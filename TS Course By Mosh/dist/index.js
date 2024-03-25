d4bbb79e8982232341fb857b5f7dce5b84b0a3e9
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2024-03-25 14:16:07 +0500

    Updated index.ts

---
+++ TS Course By Mosh/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello TypeScript!");
+let age = 10;
+if (age < 50)
+    age += 20;
+//# sourceMappingURL=index.js.map
---
+++ TS Course By Mosh/src/index.ts
@@ -0,0 +1,9 @@
+// First program
+console.log("Hello TypeScript!");
+
+// Static typing
+let age: number = 10;
+
+// TypeScript debugging
+if (age < 50)
+    age += 20;
